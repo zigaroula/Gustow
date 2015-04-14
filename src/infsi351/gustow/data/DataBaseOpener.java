76032b4d4b654bb80f12c1202e57cf225eeb2709
Marchelune <remi.sormain@gmail.com>
2015-04-14 18:51:26 +0200

    BDD progression

--- src/infsi351/gustow/data/DataBaseOpener.java
+++ src/infsi351/gustow/data/DataBaseOpener.java
@@ -8,23 +8,36 @@ import android.util.Log;
 
 public class DataBaseOpener extends SQLiteOpenHelper {
 
-	public static final String TABLE_COMMENTS = "comments";
+	public static final String TABLE_PLATS = "plats";
 	public static final String COLUMN_ID = "_id";
-	public static final String COLUMN_COMMENT = "comment";
+	public static final String COLUMN_NOM = "nom";
+	public static final String COLUMN_PRIX = "prix";
+	public static final String COLUMN_DESCRIPTION = "description";
+	public static final String COLUMN_IMAGE = "image";
+	public static final String COLUMN_TYPE = "type";
 
 	private static final String DATABASE_NAME = "commments.db";
 	private static final int DATABASE_VERSION = 1;
 
 	// Database creation sql statement
 	private static final String DATABASE_CREATE = "create table "
-			+ TABLE_COMMENTS + "(" + COLUMN_ID
-			+ " integer primary key autoincrement, " + COLUMN_COMMENT
-			+ " text not null);";
+			+ TABLE_PLATS + "(" + COLUMN_ID
+			+ " integer primary key autoincrement, " + COLUMN_NOM
+			+ " text not null " 
+			+ COLUMN_PRIX + " integer "
+			+ COLUMN_DESCRIPTION + " text not null "
+			+ COLUMN_TYPE + " text not null "
+			+ COLUMN_IMAGE + " text not null "
+			+");";
 
 	public DataBaseOpener(Context context, String name, CursorFactory factory,
 			int version) {
 		super(context, name, factory, version);
 		// TODO Auto-generated constructor stub
+	}
+
+	public DataBaseOpener(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
 
 	@Override
@@ -38,7 +51,7 @@ public class DataBaseOpener extends SQLiteOpenHelper {
 		 Log.w(DataBaseOpener.class.getName(),
 			        "Upgrading database from version " + oldVersion + " to "
 			            + newVersion + ", which will destroy all old data");
-			    db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS);
+			    db.execSQL("DROP TABLE IF EXISTS " + TABLE_PLATS);
 			    onCreate(db);
 
 	}
