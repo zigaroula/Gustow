db9c49a4f3001a180027dfd384e11d6cba7104f2
Benjamin BONTEMPS <benjamin.bontemps@telecom-paristech.fr>
2015-04-19 21:12:47 +0200

    Titre des formules

--- src/infsi351/gustow/ComposeMenu.java
+++ src/infsi351/gustow/ComposeMenu.java
@@ -123,6 +123,7 @@ public class ComposeMenu extends Activity {
 		titreMenu.setText(f.getNom());
 		Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/SnellRoundhand.ttc");
 		titreMenu.setTypeface(tf);
+		titreMenu.setTextSize(TypedValue.COMPLEX_UNIT_SP, 40);
 
 		for (TypePlat type : rubriques) {
 			
